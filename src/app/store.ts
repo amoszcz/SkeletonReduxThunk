53efbb3bbc6fd67a03cd9f9d8d563b1dc0e7872f
adam.moszczynski <adam.moszczynski@diag.pl>
2021-08-04 09:49:07 +0200

    added Redux Thunk

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,26 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import {configureStore, ThunkAction, Action, applyMiddleware, combineReducers, createStore} from '@reduxjs/toolkit';
+import counterReducer, {CounterState} from '../features/counter/counterSlice';
+import thunk from "redux-thunk";
+import {ticketsSlice, TicketsState} from "../features/Tickets/store/Tickets.store";
 
-export const store = configureStore({
-  reducer: {
+export interface AppState {
+    counter: CounterState;
+    ticketsState:TicketsState
+}
+
+export const appStateReducer = combineReducers<AppState,TypeThunkAction>({
     counter: counterReducer,
-  },
+    ticketsState:ticketsSlice.reducer
+});
+export const store = configureStore<AppState, TypeThunkAction>({
+    reducer: appStateReducer,
+    middleware: [thunk],
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppState, unknown, Action<string>>;
+export interface TypeThunkAction extends AppThunk {
+    type: void;
+}
+
